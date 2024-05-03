28d8e820a39b953a0625a05f9e89507e01cc339f
Mark Kempers <160719362+markkempers@users.noreply.github.com>
2024-05-03 10:29:43 +0200

    Allow local MYSQL connections

--- security.tf
+++ security.tf
@@ -32,11 +32,20 @@ resource "oci_core_security_list" "seclist" {
   }
   ingress_security_rules {
     protocol = local.tcp_protocol_number
-    source = "10.10.10.0/24"
+    source = var.local_subnet
     description = "Permit local SSH connections"
     tcp_options {
       min = local.ssh_port_number
       max = local.ssh_port_number
+    }
+  }
+  ingress_security_rules {
+    protocol = local.tcp_protocol_number
+    source = var.local_subnet
+    description = "Permit local MYSQL connections"
+    tcp_options {
+      min = local.mysql_port_number
+      max = local.mysql_port_number
     }
   }
   egress_security_rules {
@@ -48,6 +57,7 @@ resource "oci_core_security_list" "seclist" {
 locals {
   http_port_number          = "80"
   https_port_number         = "443"
+  mysql_port_number         = "3306"
   ssh_port_number           = "22"
   tcp_protocol_number       = "6"
   all_protocols             = "all"
